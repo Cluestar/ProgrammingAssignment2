eed59eb133f8129b1ed45c96f463937dac79bf37
Cluestar <joost.klooster72@gmail.com>
2019-08-14 14:43:48 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(z = matrix()) { #I was messing around a lot in a different Rscript altogether and accidentally renamed x into z, but I stuck with it
+  Inversed <- NULL
+  
+  sets <- function(matrix){
+    Inversed <<- NULL
+    z <<- matrix
+  }
+  
+  gets <- function()     {
+    z
+  }
+  
+  setsinversedmatrix <- function(inverses){
+    Inversed <- inverses
+  }
+  getsinversedmatrix <- function(){
+    Inversed
+  }
+  list(sets = set, gets = gets, setsinversedmatrix = setsinversedmatrix ,getsinversedmatrix = getsinversedmatrix)
+} #I thought this assignment was incredibly hard :(
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getsinversedmatrix() #I think this should inverse 'x', but I am not sure if I did it correctly. 
+  if(isTRUE(z)) {
+    return(z)
+  }
+  dat <- x$get()
+  z <- solve(dat) %*% dat
+  x$setsinversedmatrix
+  z
 }
